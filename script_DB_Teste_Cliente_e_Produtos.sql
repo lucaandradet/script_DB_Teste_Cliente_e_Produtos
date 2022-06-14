2d11a2bd219c6baaa6046c529c98e1d79912a0f1
lucaandradet <64807231+lucaandradet@users.noreply.github.com>
2022-06-14 16:32:14 -0300

    Update script_DB_Teste_Cliente_e_Produtos.sql

--- script_DB_Teste_Cliente_e_Produtos.sql
+++ script_DB_Teste_Cliente_e_Produtos.sql
@@ -6,28 +6,28 @@ USE DBExemploClientes;
 
 -- Criando tabela
 CREATE TABLE cliente (
-	clienteID int not null primary key,
-	nome varchar(50) not null,
-	endereco varchar(50) not null,
-	cidade varchar(35) not null default 'Natal',
-	estado char(02) not null default 'RN',
-	sexo char(01) not null check (sexo IN ('M', 'F')),
-	renda decimal(12,2) not null check (renda >= 750)
+	cliente_id int not null primary key,
+	nome_cliente varchar(50) not null,
+	endereco_cliente varchar(50) not null,
+	cidade_cliente varchar(35) not null default 'Natal',
+	estado_cliente char(02) not null default 'RN',
+	sexo_cliente char(01) not null check (sexo IN ('M', 'F')),
+	renda_cliente decimal(12,2) not null check (renda >= 750)
 	);
 	
-CREATE TABLE marcas
-(produto_id INT,
-descricao VARCHAR (50),
-tipo VARCHAR(2)
+CREATE TABLE marca
+(marca_id INT,
+descricao_marca VARCHAR (50),
+tipo_marca VARCHAR(2)
 );
 
 CREATE TABLE produto
 (
-cliente_id INT,
-marcas_id INT,
-nome VARCHAR(100),
-preco MONEY,
-fotos varchar(50)
+prod_cliente_id INT,
+prod_marca_id INT,
+nome_produto VARCHAR(100),
+preco_produto MONEY,
+fotos_produto varchar(50)
 );
 	
 ---
@@ -58,7 +58,7 @@ INSERT INTO cliente VALUES (11,'Márcia','Av. Ayrton Senna','Natal','RN','F',140
 INSERT INTO cliente VALUES (12,'Flávia','Rua Dantas de Sousa','Parnamirim','RN','F',1750);
 
 -- Inserindo Marcas
-INSERT INTO marcas VALUES (1,'Broto legal','A'),(2,'Prato fino','A'),(3,'Carmil','B'),(4,'Scala','B');
+INSERT INTO marca VALUES (1,'Broto legal','A'),(2,'Prato fino','A'),(3,'Carmil','B'),(4,'Scala','B');
 
 -- Inserindo Produtos
 INSERT INTO produto VALUES (1,1,'Feijão',3,'fbl.png'), (3,2,'Arroz Sinhá',5,'fbl.png'), (6,3,'Arroz Dedá',5,'fbl.png'), (9,4,'Macarrão Parafuso',3,'fbl.png');
@@ -66,26 +66,26 @@ INSERT INTO produto VALUES (1,1,'Feijão',3,'fbl.png'), (3,2,'Arroz Sinhá',5,'f
 ---
 
 -- Atualizando clientes
-UPDATE cliente SET endereco = 'Av. Maria Lacerda' where nome='Ana';
+UPDATE cliente SET endereco_cliente = 'Av. Maria Lacerda' where nome_cliente='Ana';
 
-UPDATE cliente Set cidade = 'Caicó', endereco = 'Av. Coronel Martiniano' where nome = 'Ana' or nome = 'Pedro';
+UPDATE cliente Set cidade_cliente = 'Caicó', endereco_cliente = 'Av. Coronel Martiniano' where nome_cliente = 'Ana' or nome_cliente = 'Pedro';
 
 ---
 
 -- Realizando consultas
 select * from cliente;
-select * from marcas;
+select * from marca;
 select * from produto;
 
 -- Consulta
-SELECT nome, cidade, sexo
+SELECT nome_cliente, cidade_cliente, sexo
 FROM cliente
-ORDER BY nome ASC, cidade DESC;
+ORDER BY nome_cliente ASC, cidade_cliente DESC;
 
 --Faça um relatório que exiba apenas o nome, o endereço, e a cidade do cliente da cidade de Caicó e renda menor do 6000
-SELECT nome, ENDERECO, cidade, renda 
-       FROM cliente where cidade = 'Caicó'
-	   AND renda <= 6000;
+SELECT nome_cliente, endereco_cliente, cidade_cliente, renda_cliente 
+       FROM cliente where cidade_cliente = 'Caicó'
+	   AND renda_cliente <= 6000;
 
 -- Renda entre 3000 e 5000
-SELECT * FROM cliente WHERE RENDA BETWEEN 3000 AND 5000;
+SELECT * FROM cliente WHERE renda_cliente BETWEEN 3000 AND 5000;
